14cbe5550859b270f17009b4446fa73ba610d0e9
Artur Nicolau <92381770+NicsGames008@users.noreply.github.com>
2025-03-30 02:34:50 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaa

--- Assets/Scripts/Interact/MedsInteraction.cs
+++ Assets/Scripts/Interact/MedsInteraction.cs
@@ -10,7 +10,6 @@ public class MedsInteraction : MonoBehaviour, IInteractable
         //Put funciton call here to get meds
         Debug.Log("Interacted with the a MedShed");
 
-
         Medication medication = FindObjectOfType<Medication>();
         medication.AddCurrentMedication(1);
     }
